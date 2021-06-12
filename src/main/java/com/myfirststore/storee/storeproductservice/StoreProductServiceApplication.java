7047397d843a4f16cac875e33c61bf87cf82c1f1
Andersen <mizio.spatafora@gmail.com>
2021-06-12 23:21:19 +0200

    First commit

---
+++ src/main/java/com/myfirststore/storee/storeproductservice/StoreProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myfirststore.storee.storeproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myfirststore/storee/storeproductservice/StoreProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfirststore.storee.storeproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
